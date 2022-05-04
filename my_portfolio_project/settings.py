bd8ec4b82f9d96e5e49414be9076b2a43e6f4baa
VladimirLadyzhev <vladimir.ladyzhev@gmail.com>
2022-05-04 14:26:36 +0000

    Remove old stuff

--- my_portfolio_project/settings.py
+++ my_portfolio_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-yng!kg@_n15r%fpxcnw7**++rwymvw=*n&e-vus559@ra5g)r)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['vladimirladyzhev.pythonanywhere.com']
 
 
 # Application definition
@@ -118,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = BASE_DIR / 'static'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 MEDIA_URL = 'media/'
